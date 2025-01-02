af81b39cfb4da55c3183e3f9338cc0e0a3303cbf
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-02 16:54:50 +0100

    logging middleware for logging requests & responses

---
+++ src/api/helpers/middlewares/loggingmiddleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+export function logging_middleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  log.info(
+    `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
+
+  res.on("finish", () => {
+    log.info(
+      `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
+    );
+  });
+
+  next();
+}
