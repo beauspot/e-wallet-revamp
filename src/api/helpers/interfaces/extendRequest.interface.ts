83e88bb2e6ecb844c3885a7407b6de9b8885e55e
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-02 18:14:17 +0100

    module contaning the interface for extending requests and response

---
+++ src/api/helpers/interfaces/extendRequest.interface.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+// TODO: would come back to this issue
+import { User } from "@/db/user.entity";
+
+export interface ExtendRequest extends Request {
+    user?: {
+        id?: string;
+        email?: string;
+        firstName?: string;
+        lastName?: string;
+        phoneNumber?: string;
+    }
+};
+
+export interface ExtendResponse extends Response {
+    locals: {
+        user?: User;
+    };
+}
