e1717a620997f786e64b7cf7784a4d1907ea126c
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-03 15:38:39 +0100

    added setupdb.ts module to the array of setupFilesAfterEnv option on the jes.config.ts module

--- jest.config.ts
+++ jest.config.ts
@@ -34,7 +34,7 @@ const jestConfig: JestConfigWithTsJest = {
     "^@/queues/(.*)$": ["<rootDir>/src/api/helpers/queues/$1"],
     "^@/(.*)$": ["<rootDir>/src/$1"],
   },
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup-redis.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup-redis.ts", "<rootDir>/src/__tests__/setupdb.ts"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
   },
