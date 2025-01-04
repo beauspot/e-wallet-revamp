83813b53ecc5dd73aa288a2378fc6202dee76bf4
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-04 05:20:37 +0100

    had to revamp this module adding a custom interface for sending mails which would be used for queues & workers.

--- src/api/helpers/interfaces/email.interface.ts
+++ src/api/helpers/interfaces/email.interface.ts
@@ -1,5 +1,18 @@
-export interface EmailClassConfiguration {
+interface EmailClassConfiguration {
+    sendOTPMail(): Promise<void>;
     sendWelcome(): Promise<void>;
     sendPasswordReset(): Promise<void>;
     sendPinReset(): Promise<void>;
+}
+
+interface Email {
+    from: string;
+    to: string;
+    subject: string;
+    text: string;
+}
+
+export {
+    Email,
+    EmailClassConfiguration
 }
