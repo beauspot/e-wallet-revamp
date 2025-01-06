e385a4eaf8d99872760742e634aba9a771a9ac33
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-06 07:09:21 +0100

    implemented the logic for user signing up on the controller class from the service class after injecting the users.service controller.

---
+++ src/api/controllers/users.controllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import AppError from "@/utils/appErrors";
+import { plainToInstance } from "class-transformer";
+import { ExtendRequest } from "@/interfaces/extendRequest.interface";
+
+import { UserService } from '@/services/users.service';
+
+export class UserController{
+    constructor(private userService: UserService) { }
+    
+    async registerUser(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { userData } = req.body;
+            const result = await this.userService.registerUser(userData);
+            res.status(StatusCodes.CREATED).json(result);
+        } catch (error: any) {
+            // console.error(error);
+            throw new AppError(`${error.message}`, "failed", false, StatusCodes.SERVICE_UNAVAILABLE)
+        }
+    }
+}
