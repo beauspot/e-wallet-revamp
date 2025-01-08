c23521cd8576079d73e937c30ae12778bb273e56
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-08 17:25:32 +0100

    imported the CreateVirtualAccountWorker & vanQueueEvents from the walletQueue files so their connection can be closed after testing with Jest.

--- src/__tests__/setup-redis.ts
+++ src/__tests__/setup-redis.ts
@@ -1,8 +1,19 @@
 import redisModule from "@/configs/redis.config";
+import walletQueues from "@/queues/wallet.queues";
 
 const { redisClient, shutdownClient } = redisModule;
+const { createVirtualAccountWorker, vanQueueEvents} = walletQueues;
 
 afterAll(async () => {
     await shutdownClient(redisClient);
     log.info("Redis client closed.")
+});
+
+afterAll(async () => {
+    await Promise.all([
+        createVirtualAccountWorker.close(),
+        vanQueueEvents.close()
+    ]);
+
+    log.info("BullMQ connection closed.")
 })
