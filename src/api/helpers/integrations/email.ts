8efb51131cfd2afb5aac73bd991bc2cc19c6142a
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-09 23:08:38 +0100

    setting up the email module & integrating the resend API for sending mails

--- src/api/helpers/integrations/email.ts
+++ src/api/helpers/integrations/email.ts
@@ -1,96 +1,65 @@
-import * as nodemailer from "nodemailer";
-import otpgenerator from "otp-generator";
-import { EmailClassConfiguration } from "@/interfaces/email.interface";
+import { Resend } from "resend";
 
-import redisDB from "@/configs/redis.config";
+// import { baseTemplate } from '@/templates/baseTemplate';
+import { EmailJobData } from "@/interfaces/email.interface";
+import { forgotPassword } from '@/templates/forgotPassword';
+import { forgotTransactionPin } from "@/templates/forgotTransactionPin";
+import { resetPassword } from '@/templates/resetPassword';
+import { resetTransactionPin } from "@/templates/resetTransactionPin";
+import { welcomeEmail } from '@/templates/welcomeEmail';
 
-const { redisClient } = redisDB;
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
-interface User {
-    email: string;
-    firstName: string;
-}
+const TEMPLATES = {
+    resetPassword: {
+        subject: 'Password Reset Successful',
+        from: 'WalletHub Customer Support <donotreply@wallethub.com>',
+        template: resetPassword,
+    },
+    forgotPassword: {
+        subject: 'Reset Your Password',
+        from: 'WalletHub Customer Support <donotreply@wallethub.com>',
+        template: forgotPassword,
+    },
+    welcomeEmail: {
+        subject: 'Welcome to WalletHub',
+        from: 'WalletHub Customer Support <donotreply@wallethub.com>',
+        template: welcomeEmail,
+    },
+    forgotTransactionPin: {
+        subject: 'Forgot TransactionPin',
+        from: 'WalletHub Customer Support <donotreply@wallet.com>',
+        template: forgotTransactionPin,
+    },
+    resettransactionPin: {
+        subject: 'Transaction Pin Reset Successfully',
+        from: 'WalletHub Customer Support <donotreply@wallethub.com>',
+        template: resetTransactionPin,
+    },
+};
 
-export class EmailService implements EmailClassConfiguration {
-    private to: string;
-    private firstName: string;
-    private resetToken: string;
-    private from: string;
-    private otp: string;
+ const sendEmail = async (job: EmailJobData) => {
+    const { data, type } = job as EmailJobData;
+    const options = TEMPLATES[type];
 
-    constructor(user: User, resetToken: string, otp: string) {
-        this.to = user.email;
-        this.firstName = user.firstName;
-        this.resetToken = resetToken;
-        this.from = ` WalletHub: <${process.env.EMAIL_FROM}>`;
-        this.otp = otp;
-    }
-
-    private async generateAndSaveOTP(): Promise<string> {
-        // Generate a 6-digit numeric OTP
-        const otp = otpgenerator.generate(6, { upperCaseAlphabets: false, specialChars: false })
-        
-        // Save the OTP in Redis with a 5-minute expiration
-        await redisClient.setex(`otp:${this.to}`, 300, otp); // key expires in 300seconds 
-
-        return otp;
-    };
-
-    async __sendOTPMail__(): Promise<void> {
-        // Generate & save otp
-        const otp = await this.generateAndSaveOTP();
-
-        // Email content 
-        const template = `<p>Your One-Time Password to verify your email is <strong>${otp}</strong>. Note: It is valid for only 5 minutes.</p>`;
-
-        // Send the email
-        await this.send("Verification OTP", template);
-    }
-
-    private newTransport() {
-        return nodemailer.createTransport({
-            service: process.env.EMAIL_SERVICE || "Gmail",
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD,
-            },
+    log.info('job send email', job);
+    log.info('options', options);
+    try {
+        const dispatch = await resend.emails.send({
+            from: options.from,
+            to: data.to,
+            subject: options.subject,
+            html: options.template(data),
         });
-    }
 
-    // Send the actual email
-    async send(subject: string, template: string) {
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html: template,
-        };
-        await this.newTransport().sendMail(mailOptions);
+        log.info(`Resend api successfully delivered ${type} email to ${data.to}`);
+        return dispatch;
+    } catch (error) {
+        log.error(`Resend api failed to deliver ${type} email to ${data.to}` + error);
     }
+};
 
-    // This are just examples of sending mails to the user
-
-    async sendOTPMail(): Promise<void> {
-        await this.send(
-            "Verification OTP",
-            `<p>Your One-Time Password to verify your email is <strong>${this.otp}</strong>. Note: It is valid for only 30 seconds.</p>`
-        );
-    }
-
-    async sendWelcome(): Promise<void> {
-        await this.send("Welcome", `<h1>Welcome to WalletHub, ${this.firstName}!</h1>`);
-    }
-
-    async sendPasswordReset():Promise<void>  {
-        await this.send(
-            "Password Reset OTP",
-            `<p>Forgot your password? Your One-Time Password to reset your password is <strong>${this.resetToken}</strong>. It is valid for only 10 minutes.</p>`
-        );
-    }
-    async sendPinReset(): Promise<void> {
-        await this.send(
-            "Pin Reset OTP",
-            `<p>Forgot your transaction pin? Your One-Time Password to reset your transaction pin is <strong>${this.resetToken}</strong>. It is valid for only 10 minutes.</p>`
-        );
-    }
+export default {
+    TEMPLATES,
+    sendEmail
 };
