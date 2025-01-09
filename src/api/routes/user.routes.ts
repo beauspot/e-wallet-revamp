ae70a411a58fa3fcf2256703d88392adfff1c049
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-09 23:15:32 +0100

    created the endpoint for verifying the user's otp

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -7,23 +7,19 @@ import { UserWallet } from "@/db/wallet.entity";
 import { UserService } from "@/services/users.service";
 import { validate } from "@/helpers/middlewares/validate";
 import { UserController } from "@/controllers/users.controllers";
-import { Flw } from "@/api/helpers/integrations/flutterwave";
 
 import { createUserSchema, loginUserSchema } from "@/schemas/user.schema";
 
 // Dependency injection
-const flw = new Flw(
-    process.env.FLUTTERWAVE_PUBLIC_KEY,
-    process.env.FLUTTERWAVE_SECRET_KEY,
-    process.env.FLUTTERWAVE_ENCRYPTION_KEY,
-    UserTransactionModel,
-)
 const user_service = new UserService(User, UserWallet);
 const user_controller = new UserController(user_service);
 
 
 const router = Router();
-router.route("/register").post((req: Request, res: Response, next: NextFunction) => user_controller.registerUser(req, res, next))
+router.route("/register").post((req: Request, res: Response, next: NextFunction) => user_controller.registerUser(req, res, next));
+
+router.route("/verify-otp").post((req: Request, res: Response, next: NextFunction) => user_controller.verifyOTP(req, res, next));
+
 
 
 export default router
