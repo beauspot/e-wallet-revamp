fb5ae50d93b0adecd2ecfa806eb85615c061ae0e
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-11 21:42:51 +0100

    added the SendMailWorker & mailQueueEvents to the module so they could be shutdown after the tests closes.

--- src/__tests__/setup-redis.ts
+++ src/__tests__/setup-redis.ts
@@ -1,8 +1,10 @@
 import redisModule from "@/configs/redis.config";
 import walletQueues from "@/queues/wallet.queues";
+import emailQueues from "@/queues/email.queues";
 
 const { redisClient, shutdownClient } = redisModule;
-const { createVirtualAccountWorker, vanQueueEvents} = walletQueues;
+const { createVirtualAccountWorker, vanQueueEvents } = walletQueues;
+const { SendMailWorker, mailQueueEvents } = emailQueues;
 
 afterAll(async () => {
     await shutdownClient(redisClient);
@@ -12,7 +14,9 @@ afterAll(async () => {
 afterAll(async () => {
     await Promise.all([
         createVirtualAccountWorker.close(),
-        vanQueueEvents.close()
+        vanQueueEvents.close(),
+        SendMailWorker.close(),
+        mailQueueEvents.close()
     ]);
 
     log.info("BullMQ connection closed.")
