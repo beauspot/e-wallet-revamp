9616e723588d96b50e7938d98476dcd77286a617
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-11 21:12:00 +0100

    integrated the logout endpoint from the corresponding controller

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -16,12 +16,16 @@ const user_controller = new UserController(user_service);
 
 
 const router = Router();
+
+
 router.route("/register").post((req: Request, res: Response, next: NextFunction) => user_controller.registerUser(req, res, next));
 
 router.route("/verify-otp").post((req: Request, res: Response, next: NextFunction) => user_controller.verifyOTP(req, res, next));
 
 router.route("/login").post((req: Request, res: Response, next: NextFunction) => user_controller.LoginUser(req, res, next));
 
+router.use(protect);
 
+router.route("/logout").post((req:Request, res: Response, next: NextFunction) => user_controller.LogoutUser(req, res, next));
 
 export default router
