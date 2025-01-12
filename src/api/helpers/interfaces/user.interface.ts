b56cb23d207968199a58bae38520cc2a7680e08f
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-12 21:18:00 +0100

    took out unnecessary interface

--- src/api/helpers/interfaces/user.interface.ts
+++ src/api/helpers/interfaces/user.interface.ts
@@ -1,6 +1,4 @@
-import { Express } from "express";
 import { User } from "@/db/user.entity";
-import { UserWallet } from "@/db/wallet.entity";
 import { gender_enum, userRole } from "@/enum/user.enum";
 import { VerificationInstance } from "twilio/lib/rest/verify/v2/service/verification";
 
@@ -19,23 +17,18 @@ interface userInterface {
   date_of_birth: Date;
 }
 
-interface DecodedToken {
-  id: string;
-  iat: number;
-};
 
 interface UserSercviceInterface {
   registerUser(userData: Partial<userInterface>): Promise<{ user: User }>;
   verifyEmailOTP(email: string, otp: string): Promise<boolean>;
   loginUser(identifier: string, password: string): Promise<User>;
-  // createTransactionPin(pin: string): Promise<{ userWallet: string }>;
-  // forgotPassword(email: string): Promise<string>;
+  logout(session:Express.Session): Promise<void>;
+  // forgotPassword(identifier: string): Promise<string>;
   // forgotTransactionPin(email: string): Promise<string>;
   // resetPassword(email: string, otp: string, newPassword: string): Promise<string>;
   // resetTransactionPin(email: string, otp: string, newPin: string): Promise<string>;
   // updatePassword(userId: string, currentPassword: string, newPassword: string): Promise<string>;
   // updateTransactionPin(userId: string, currentPin: string, newPin: string): Promise<string>;
-  logout(session:Express.Session): Promise<void>;
 };
 
-export { userInterface, DecodedToken, UserSercviceInterface, VerificationInstance };
+export { userInterface, UserSercviceInterface, VerificationInstance };
