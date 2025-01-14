99292567693b42ff12c642c9e3f22999391afe60
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-14 23:27:43 +0100

    module for setting up a middleware for user's to authorize transactions

---
+++ src/api/helpers/middlewares/auth_transaction_.ts
@@ -0,0 +1,7 @@
+/* 
+This middleware identifies the user,
+ checks if the user is authenticated.
+    compares the hash of the user's transaction pin
+    if the hash passes, transfers should be authorized 
+    else transfer cannot be authorized.
+*/
