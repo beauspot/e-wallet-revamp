379ef594934bde1c5c715cd861745aa19bf5483c
beauspot <42305168+beauspot@users.noreply.github.com>
2025-01-14 11:21:00 +0100

    took out the changePin method from the interface as it was not needed since it only existed on the User endpoint.

--- src/api/helpers/interfaces/wallet.interface.ts
+++ src/api/helpers/interfaces/wallet.interface.ts
@@ -2,7 +2,7 @@ import { UserWallet } from "@/db/wallet.entity";
 import { UserTransactionModel } from "@/db/transactions.entity";
 import { CardChargePayload, TransferPayload, AuthorizeCardPaymentPayload } from "@/interfaces/flutterwave.interface";
 
-export interface SessionData {
+export interface SessionData  {
     reCallCharge?: {
         data?: {
             flw_ref?: string;
@@ -13,7 +13,6 @@ export interface SessionData {
 export interface WalletServiceInterface {
     getWallet(userId: string): Promise<UserWallet>;
     getBalance(userId: string): Promise<number>;
-    changePin(userId: string, oldPin: string, newPin: string): Promise<UserWallet>;
     deposit(payload: CardChargePayload, userEmail: string): Promise<UserTransactionModel>;
     authorize(payload: AuthorizeCardPaymentPayload, sessionData: SessionData): Promise<SessionData>;
     transfer(payload: TransferPayload, userId: string): Promise<UserTransactionModel>;
