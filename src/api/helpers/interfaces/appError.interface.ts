657f33be4d8bf49de18c8871e71fa35dc6332817
beauspot <42305168+beauspot@users.noreply.github.com>
2025-02-27 11:59:57 +0100

    removed the unsed import statments.

--- src/api/helpers/interfaces/appError.interface.ts
+++ src/api/helpers/interfaces/appError.interface.ts
@@ -2,11 +2,11 @@ interface AppErrorInterface extends Error {
     status?: string;
     isOperational?: boolean;
     statusCode?: number;
-};
+}
 
 interface AppErrorConstructor {
-    new(messae: string, status?: string, isOperational?: boolean, statusCode?: number):AppErrorInterface
-};
+    new(message: string, status?: string, isOperational?: boolean, statusCode?: number): AppErrorInterface
+}
 
 declare global {
     var AppError: AppErrorConstructor;
