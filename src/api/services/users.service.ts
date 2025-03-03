866a577e9c8be1c2b8797de4ca98cb5d89059ae4
beauspot <42305168+beauspot@users.noreply.github.com>
2025-03-03 15:11:03 +0100

    eslint/prettier integrated.

--- src/api/services/users.service.ts
+++ src/api/services/users.service.ts
@@ -1,132 +1,133 @@
+/* eslint-disable prettier/prettier */
 import * as bcrypt from "bcryptjs";
-import crypto from "crypto"
+import crypto from "crypto";
 import { Service } from "typedi";
 
-import redisModule from "@/configs/redis.config";
 import { AppDataSource } from "@/configs/db.config";
+import redisModule from "@/configs/redis.config";
+import { User } from "@/db/user.entity";
 import { EmailJobData } from "@/interfaces/email.interface";
 import { userInterface, UserSercviceInterface } from "@/interfaces/user.interface";
-
-import { User } from "@/db/user.entity";
-
-// import Queues
-import WalletQueue from '@/queues/wallet.queues';
 import emailQueues from "@/queues/email.queues";
+// import Queues
+import WalletQueue from "@/queues/wallet.queues";
 
 const { redisClient } = redisModule;
 const { addMailToQueue } = emailQueues;
 
-
 @Service()
 export class UserService implements UserSercviceInterface {
-    constructor(private userEntity: typeof User) { }
+  // eslint-disable-next-line no-unused-vars
+  constructor(private userEntity: typeof User) {}
 
-    IV_LENGTH = 16;
+  IV_LENGTH = 16;
 
-    private async hashPassword(password: string): Promise<string> {
+  private async hashPassword(password: string): Promise<string> {
+    const saltRounds = 12;
+    return bcrypt.hash(password, saltRounds);
+  }
 
-        const saltRounds = 12;
-        return bcrypt.hash(password, saltRounds);
+  private async verifyPassword(inputPassword: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(inputPassword, hashedPassword);
+  }
+
+  private async hashOtp(otp: string): Promise<string> {
+    const saltRounds = 10;
+    return bcrypt.hash(otp, saltRounds);
+  }
+
+  private async verifyOtpHash(inputOtp: string, hashedOtp: string): Promise<boolean> {
+    return bcrypt.compare(inputOtp, hashedOtp);
+  }
+
+  async registerUser(userData: Partial<userInterface>) {
+    if (!userData.password) {
+      throw new AppError("Password Required", "failed", false, 401);
+    }
+
+    const hashedPassword = await this.hashPassword(userData.password);
+
+    const userRepository = AppDataSource.getRepository(this.userEntity).create({
+      ...userData,
+      password: hashedPassword
+    });
+
+    const savedUser = await AppDataSource.getRepository(this.userEntity).save(userRepository);
+    // log.info(savedUser);
+
+    // Add the VAN creation job to the queue
+    await WalletQueue.addVANToQueue(savedUser.id);
+
+    // Generate OTP and hash it
+    const OTP_EXPIRY_SECONDS = 300;
+    const otp = crypto.randomInt(100000, 999999).toString();
+    const hashedOtp = await this.hashOtp(otp);
+
+    // Save OTP in redis with expiry
+    await redisClient.setex(`otp:${savedUser.email}`, OTP_EXPIRY_SECONDS, hashedOtp);
+
+    const emailJobData: EmailJobData = {
+      type: "welcomeEmail",
+      data: {
+        to: savedUser.email,
+        firstName: savedUser.firstname,
+        otp,
+        priority: "high"
+      }
     };
 
-    private async verifyPassword(inputPassword: string, hashedPassword: string): Promise<boolean> {
-        return bcrypt.compare(inputPassword, hashedPassword);
+    await addMailToQueue(emailJobData);
+    // log.info(mailingQueue);
+
+    // log.info(savedWallet);
+
+    return { user: savedUser };
+  }
+
+  async verifyEmailOTP(email: string, otp: string): Promise<boolean> {
+    const hashedOtp = await redisClient.get(`otp:${email}`);
+
+    if (!hashedOtp) {
+      throw new AppError("Invalid OTP or OTP expired", "failed", false, 400);
     }
 
-    private async hashOtp(otp: string): Promise<string> {
-        const saltRounds = 10;
-        return bcrypt.hash(otp, saltRounds);
+    const isMatch = await this.verifyOtpHash(otp, hashedOtp);
+
+    if (!isMatch) {
+      throw new AppError("Invalid OTP");
     }
 
-    private async verifyOtpHash(inputOtp: string, hashedOtp: string): Promise<boolean> {
-        return bcrypt.compare(inputOtp, hashedOtp);
+    // Reset OTP expiry
+    await redisClient.expire(`otp:${email}`, 0);
+
+    return true;
+  }
+
+  async loginUser(identifier: string, password: string) {
+    if (!identifier || !password) {
+      throw new AppError("Provide phone or email and password!", "failed", false, 400);
     }
 
-    async registerUser(userData: Partial<userInterface>) {
+    const user = await AppDataSource.getRepository(this.userEntity).findOne({
+      where: [{ email: identifier }, { phonenumber: identifier }],
+      select: ["id", "password"]
+    });
 
-            if (!userData.password) throw new AppError("Password Required", "failed", false, 401);
-
-            const hashedPassword = await this.hashPassword(userData.password);
-
-            const userRepository = AppDataSource.getRepository(this.userEntity).create({
-                ...userData,
-                password: hashedPassword,
-            });
-
-            const savedUser = await AppDataSource.getRepository(this.userEntity).save(userRepository);
-            // log.info(savedUser);
-
-            // Add the VAN creation job to the queue
-        await WalletQueue.addVANToQueue(savedUser.id);
-
-            // Generate OTP and hash it
-            const OTP_EXPIRY_SECONDS = 300;
-            const otp = crypto.randomInt(100000, 999999).toString();
-            const hashedOtp = await this.hashOtp(otp);
-
-            // Save OTP in redis with expiry 
-            await redisClient.setex(`otp:${savedUser.email}`, OTP_EXPIRY_SECONDS, hashedOtp);
-
-            const emailJobData: EmailJobData = {
-                type: "welcomeEmail",
-                data: {
-                    to: savedUser.email,
-                    firstName: savedUser.firstname,
-                    otp,
-                    priority: "high"
-                }
-            };
-
-            await addMailToQueue(emailJobData);
-            // log.info(mailingQueue);
-
-            // log.info(savedWallet);
-
-        return { user: savedUser };
-
-    };
-
-    async verifyEmailOTP(email: string, otp: string): Promise<boolean> {
-
-            const hashedOtp = await redisClient.get(`otp:${email}`);
-
-            if (!hashedOtp) throw new AppError("Invalid OTP or OTP expired", "failed", false, 400);
-
-            const isMatch = await this.verifyOtpHash(otp, hashedOtp);
-
-            if (!isMatch) throw new AppError("Invalid OTP");
-
-            // Reset OTP expiry
-            await redisClient.expire(`otp:${email}`, 0);
-
-            return true;
-
+    if (!user || !(await this.verifyPassword(password, user.password))) {
+      throw new AppError("Incorrect email/phone number or password", "failed", false, 401);
     }
 
-    async loginUser(identifier: string, password: string) {
+    return user;
+  }
 
-        if (!identifier || !password) throw new AppError("Provide phone or email and password!", "failed", false, 400);
-
-            const user = await AppDataSource.getRepository(this.userEntity).findOne({
-                where: [
-                    { email: identifier },
-                    { phonenumber: identifier },
-    
-            ], select: ['id', "password"] });
-
-            if (!user || !(await this.verifyPassword(password, user.password)))
-                throw new AppError("Incorrect email/phone number or password", "failed", false, 401);
-
-            return user;
-
-    }
-
-    async logout(session: Express.Session): Promise<void> {
-        return new Promise((resolve, reject) => { 
-            session.destroy((error: any) => {
-                if (error) return reject(new AppError("Logout Failed.", "failed", false, 500));
-                resolve()
-            })
-        })
-    }
+  async logout(session: Express.Session): Promise<void> {
+    return new Promise((resolve, reject) => {
+      session.destroy((error: any) => {
+        if (error) {
+          return reject(new AppError("Logout Failed.", "failed", false, 500));
+        }
+        resolve();
+      });
+    });
+  }
 }
--- src/api/services/wallet.service.ts
+++ src/api/services/wallet.service.ts
@@ -1,129 +1,145 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+import bcrypt from "bcryptjs";
 import { Service } from "typedi";
 
-import bcrypt from "bcryptjs";
-
-import { User } from "@/db/user.entity";
-import AppError from "@/utils/appErrors";
-import { UserWallet } from "@/db/wallet.entity";
-import { AppDataSource } from "@/configs/db.config";
-import { generateReference } from "@/utils/generateRef";
 import { FlutterwaveService as Flw } from "@/api/helpers/integrations/flutterwave";
+import { AppDataSource } from "@/configs/db.config";
 import { UserTransactionModel } from "@/db/transactions.entity";
+import { User } from "@/db/user.entity";
+import { UserWallet } from "@/db/wallet.entity";
+import { TransactionType, TransactionStatus, PaymentType } from "@/enum/transactions.enum";
+import {
+  CardChargePayload,
+  TransferPayload,
+  AuthorizeCardPaymentPayload
+} from "@/interfaces/flutterwave.interface";
 import { WalletServiceInterface, SessionData } from "@/interfaces/wallet.interface";
-import { TransactionType, TransactionStatus, PaymentType } from "@/enum/transactions.enum"
-import { CardChargePayload, TransferPayload, AuthorizeCardPaymentPayload } from "@/interfaces/flutterwave.interface";
+import AppError from "@/utils/appErrors";
+import { generateReference } from "@/utils/generateRef";
 
 @Service()
 export class WalletService implements WalletServiceInterface {
-    constructor(private wallet: typeof UserWallet, private flw: Flw, private transaction: typeof UserTransactionModel, private user: typeof User) { }
+  constructor(
+    private wallet: typeof UserWallet,
+    private flw: Flw,
+    private transaction: typeof UserTransactionModel,
+    private user: typeof User
+  ) {}
 
-    async getWallet(userId: string): Promise<UserWallet> {
-     
-            const walletRepo = AppDataSource.getRepository(this.wallet);
-            const wallet = await walletRepo.findOne({
-                where: { user: { id: userId } },
-                relations: ["user"]
-            });
+  async getWallet(userId: string): Promise<UserWallet> {
+    const walletRepo = AppDataSource.getRepository(this.wallet);
+    const wallet = await walletRepo.findOne({
+      where: { user: { id: userId } },
+      relations: ["user"]
+    });
 
-            if (!wallet) throw new AppError(`Wallet Not found Please contact administrator`, "Failed", false, 400);
-
-            return wallet;
-    };
-
-    async getBalance(userId: string): Promise<number>{
-     
-            const walletRepository = AppDataSource.getRepository(this.wallet);
-            const wallet = await walletRepository.findOne({
-                where: { user: { id: userId } },
-                relations: ["user"]
-            });
-
-            if (!wallet) throw new AppError(
-                `You don't have a wallet. Please contact the administrator`,
-                "false", false, 400
-            );
-
-            return wallet.balance;
-    };
-
-    async deposit(payload: CardChargePayload, userEmail: string): Promise<UserTransactionModel> {
-        
-            payload.email = payload.email || userEmail;
-            payload.tx_ref = generateReference("transaction");
-            payload.enckey = process.env.FLUTTERWAVE_ENCRYPTION_KEY;
-
-            const response = await this.flw.chargeCard(payload);
-            return response;
-     
+    if (!wallet) {
+      throw new AppError("Wallet Not found Please contact administrator", "Failed", false, 400);
     }
 
-    async authorize(payload: AuthorizeCardPaymentPayload, sessionData: SessionData): Promise<SessionData> {
-        
-            payload.flw_ref = sessionData?.reCallCharge?.data?.flw_ref || payload.flw_ref;
-            const response = await this.flw.authorizeCardPayment(payload);
-            return response;
+    return wallet;
+  }
+
+  async getBalance(userId: string): Promise<number> {
+    const walletRepository = AppDataSource.getRepository(this.wallet);
+    const wallet = await walletRepository.findOne({
+      where: { user: { id: userId } },
+      relations: ["user"]
+    });
+
+    if (!wallet) {
+      throw new AppError(
+        "You don't have a wallet. Please contact the administrator",
+        "false",
+        false,
+        400
+      );
+    }
+
+    return wallet.balance;
+  }
+
+  async deposit(payload: CardChargePayload, userEmail: string): Promise<UserTransactionModel> {
+    payload.email = payload.email || userEmail;
+    payload.tx_ref = generateReference("transaction");
+    payload.enckey = process.env.FLUTTERWAVE_ENCRYPTION_KEY;
+
+    const response = await this.flw.chargeCard(payload);
+    return response;
+  }
+
+  async authorize(
+    payload: AuthorizeCardPaymentPayload,
+    sessionData: SessionData
+  ): Promise<SessionData> {
+    payload.flw_ref = sessionData?.reCallCharge?.data?.flw_ref || payload.flw_ref;
+    const response = await this.flw.authorizeCardPayment(payload);
+    return response;
+  }
+
+  async transfer(payload: TransferPayload, userId: string): Promise<UserTransactionModel> {
+    const walletRepository = AppDataSource.getRepository(this.wallet);
+    const wallet = await walletRepository.findOne({
+      where: { user: { id: userId } },
+      relations: ["user"]
+    });
+    if (!wallet) {
+      throw new AppError("Wallet not found", "false", false, 404);
+    }
+
+    const userRepository = AppDataSource.getRepository(this.user);
+    const user = await userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new AppError("User not found", "false", false, 401);
+    }
+
+    const validPin = bcrypt.compare(payload.transactionPin, user.transaction_pin);
+    if (!validPin) {
+      throw new AppError("Invalid transaction pin", "false", false, 401);
+    }
+
+    if (wallet.balance < payload.amount || wallet.balance - payload.amount <= 100) {
+      throw new AppError("Insufficient funds or minimum balance required", "false", false, 400);
+    }
+
+    const details = {
+      account_number: payload.account_no,
+      account_bank: payload.bank
     };
 
-    async transfer(payload: TransferPayload, userId: string): Promise<UserTransactionModel> {
-    
-            const walletRepository = AppDataSource.getRepository(this.wallet);
-            const wallet = await walletRepository.findOne({
-                where: { user: { id: userId } },
-                relations: ["user"]
-            });
-            if (!wallet) throw new AppError("Wallet not found", "false", false, 404);
+    await this.flw.verifyAccount(details);
 
-       
-            const userRepository = AppDataSource.getRepository(this.user);
-            const user = await userRepository.findOne({ where: { id: userId } });
+    const transferPayload = {
+      ...payload,
+      reference: generateReference("transfer"),
+      callback_url: `${process.env.APP_BASE_URL}/api/v1/wallet/transfer/verify`
+    };
 
-            if (!user)
-                throw new AppError("User not found", "false", false, 401);
+    const response = await this.flw.transfer(transferPayload);
 
-            const validPin = bcrypt.compare(payload.transactionPin, user.transaction_pin);
-            if (!validPin) throw new AppError("Invalid transaction pin", "false", false, 401);
+    const transaction = {
+      reference: response.data.reference,
+      gatewayReference: response.data.flw_ref || "N/A",
+      transactionType: TransactionType.Debit,
+      paymentType: PaymentType.Account,
+      amount: response.data.amount,
+      currency: response.data.currency,
+      recipient: response.data.account_number.replace(/(?<=.{4})./g, "*"),
+      description: response.data.narration,
+      user: { id: userId },
+      status:
+        response.data.status === "NEW"
+          ? TransactionStatus.Pending
+          : response.data.status === "FAILED"
+            ? TransactionStatus.Failed
+            : TransactionStatus.Successful
+    };
 
-            if (wallet.balance < payload.amount || wallet.balance - payload.amount <= 100) {
-                throw new AppError("Insufficient funds or minimum balance required", "false", false, 400);
-            }
-
-            const details = {
-                account_number: payload.account_no,
-                account_bank: payload.bank,
-            };
-
-            await this.flw.verifyAccount(details);
-
-            const transferPayload = {
-                ...payload,
-                reference: generateReference("transfer"),
-                callback_url: `${process.env.APP_BASE_URL}/api/v1/wallet/transfer/verify`,
-            };
-
-            const response = await this.flw.transfer(transferPayload);
-
-
-            const transaction = {
-                reference: response.data.reference,
-                gatewayReference: response.data.flw_ref || "N/A",
-                transactionType: TransactionType.Debit,
-                paymentType: PaymentType.Account,
-                amount: response.data.amount,
-                currency: response.data.currency,
-                recipient: response.data.account_number.replace(/(?<=.{4})./g, "*"),
-                description: response.data.narration,
-                user: { id: userId },
-                status: response.data.status === "NEW"
-                    ? TransactionStatus.Pending
-                    : response.data.status === "FAILED"
-                        ? TransactionStatus.Failed
-                        : TransactionStatus.Successful,
-            };
-
-            const transactionRepo = AppDataSource.getRepository(this.transaction);
-            const wallet_transaction = transactionRepo.create(transaction)
-            await transactionRepo.save(wallet_transaction);
-            return wallet_transaction;
-
-    }
+    const transactionRepo = AppDataSource.getRepository(this.transaction);
+    const wallet_transaction = transactionRepo.create(transaction);
+    await transactionRepo.save(wallet_transaction);
+    return wallet_transaction;
+  }
 }
