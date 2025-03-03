de3dca0fbb51cc618994014a851e223c408586e0
beauspot <42305168+beauspot@users.noreply.github.com>
2025-03-03 14:51:51 +0100

    eslint/prettier integrated.

--- src/api/controllers/users.controllers.ts
+++ src/api/controllers/users.controllers.ts
@@ -1,73 +1,73 @@
-import {  RequestHandler } from "express";
+import { RequestHandler } from "express";
 import AsyncHandler from "express-async-handler";
-import { StatusCodes } from "http-status-codes";
-import { plainToInstance } from "class-transformer";
-import { ExtendRequest } from "@/interfaces/extendRequest.interface";
 
-import { UserService } from '@/services/users.service';
+import { UserService } from "@/services/users.service";
+import { StatusCodes } from "http-status-codes";
 
 export class UserController {
-    constructor(private userService: UserService) { }
-    
-    registerUser: RequestHandler = AsyncHandler(async (req, res, next) => {
+  // eslint-disable-next-line no-unused-vars
+  constructor(private userService: UserService) {}
 
-            const { userData } = req.body;
-            const result = await this.userService.registerUser(userData);
-        res.status(StatusCodes.CREATED).json({
-            status: "Success",
-            data: result
-        });
+  registerUser: RequestHandler = AsyncHandler(async (req, res) => {
+    const { userData } = req.body;
+    const result = await this.userService.registerUser(userData);
+    res.status(StatusCodes.CREATED).json({
+      status: "Success",
+      data: result
+    });
+  });
 
+  verifyOTP: RequestHandler = AsyncHandler(async (req, res) => {
+    const { email, otp } = req.body;
+    await this.userService.verifyEmailOTP(email, otp);
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      message: "OTP verified successfully"
+    });
+  });
+
+  LoginUser: RequestHandler = AsyncHandler(async (req, res): Promise<void> => {
+    const { phoneNumber, email, password } = req.body;
+
+    const identifier = phoneNumber || email;
+    const user = await this.userService.loginUser(identifier, password);
+
+    if (!req.session) {
+      throw new AppError(
+        "Session is not available",
+        "failed",
+        false,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+
+    req.session.userId = user.id;
+    req.session.isLoggedIn = true;
+
+    res.status(StatusCodes.OK).json({
+      message: "Login Successful",
+      user: {
+        id: user.id,
+        email: user.email,
+        firstName: user.firstname,
+        middleName: user.middlename,
+        lastName: user.lastname,
+        role: user.role
+      }
     });
 
-    verifyOTP: RequestHandler = AsyncHandler(async (req, res, next) => {
+    return;
+  });
 
-        const { email, otp } = req.body;
-        await this.userService.verifyEmailOTP(email, otp);
-        res.status(StatusCodes.OK).json({
-            status: 'success',
-            message: "OTP verified successfully"
-        });
+  LogoutUser: RequestHandler = AsyncHandler(async (req, res): Promise<void> => {
+    if (!req.session) {
+      throw new AppError("No active session found", "failed", false, StatusCodes.BAD_REQUEST);
+    }
 
-    })
+    await this.userService.logout(req.session);
 
-    LoginUser: RequestHandler = AsyncHandler(async (req, res, next): Promise<void> => {
-
-        const { phoneNumber, email, password } = req.body;
-
-            const identifier = phoneNumber || email;
-            const user = await this.userService.loginUser(identifier, password);
-
-        if (!req.session) 
-                throw new AppError("Session is not available", "failed", false, StatusCodes.INTERNAL_SERVER_ERROR);
-
-            req.session.userId = user.id;
-            req.session.isLoggedIn = true
-
-            res.status(StatusCodes.OK).json({
-                message: "Login Successful",
-                user: {
-                    id: user.id,
-                    email: user.email,
-                    firstName: user.firstname,
-                    middleName: user.middlename,
-                    lastName: user.lastname,
-                    role: user.role
-                }
-            });
-
-            return;
-    })
-
-    LogoutUser: RequestHandler = AsyncHandler(async (req, res, next): Promise<void> => {
-        
-            if (!req.session) throw new AppError("No active session found", "failed", false, StatusCodes.BAD_REQUEST);
-
-            await this.userService.logout(req.session);
-
-            res.status(StatusCodes.OK).json({
-                message: "Successfully logged out"
-            })
-        
-    })
+    res.status(StatusCodes.OK).json({
+      message: "Successfully logged out"
+    });
+  });
 }
--- src/api/controllers/wallet.controllers.ts
+++ src/api/controllers/wallet.controllers.ts
@@ -1,66 +1,68 @@
-import { StatusCodes } from "http-status-codes";
+import { RequestHandler } from "express";
 import AsyncHandler from "express-async-handler";
-import { plainToInstance } from "class-transformer";
-import {  RequestHandler } from "express";
 
-import {  SessionData } from "@/interfaces/wallet.interface";
-import { WalletService } from "@/services/wallet.service";
 import { ExtendRequest } from "@/interfaces/extendRequest.interface";
-
+import { SessionData } from "@/interfaces/wallet.interface";
+import { WalletService } from "@/services/wallet.service";
+import { StatusCodes } from "http-status-codes";
 
 export class WalletController {
-    constructor(private walletService: WalletService) {}
+  // eslint-disable-next-line no-unused-vars
+  constructor(private walletService: WalletService) {}
 
-    getWallet: RequestHandler = AsyncHandler(async (req: ExtendRequest, res, next) => {
-        if (!req.user?.id)
-            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+  getWallet: RequestHandler = AsyncHandler(async (req: ExtendRequest, res) => {
+    if (!req.user?.id) {
+      throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+    }
 
-        const wallet = await this.walletService.getWallet(req.user.id);
-        res.status(StatusCodes.OK).json({
-            status: "Success",
-            data: wallet
-        });
+    const wallet = await this.walletService.getWallet(req.user.id);
+    res.status(StatusCodes.OK).json({
+      status: "Success",
+      data: wallet
     });
+  });
 
-    getBalance: RequestHandler = AsyncHandler(async (req: ExtendRequest, res, next) => {
-        if (!req.user?.id)
-            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
-        const balance = await this.walletService.getBalance(req.user.id);
-        res.status(StatusCodes.OK).json({
-            status: "Success",
-            data: balance
-        });
+  getBalance: RequestHandler = AsyncHandler(async (req: ExtendRequest, res) => {
+    if (!req.user?.id) {
+      throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+    }
+    const balance = await this.walletService.getBalance(req.user.id);
+    res.status(StatusCodes.OK).json({
+      status: "Success",
+      data: balance
     });
+  });
 
-    deposit: RequestHandler = AsyncHandler(async (req: ExtendRequest, res, next) => {
-        if (!req.user?.email)
-            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
-        const response = await this.walletService.deposit(req.body, req.user.email);
-            res.status(200).json({
-                status: "Success",
-                data: response
-            });
+  deposit: RequestHandler = AsyncHandler(async (req: ExtendRequest, res) => {
+    if (!req.user?.email) {
+      throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+    }
+    const response = await this.walletService.deposit(req.body, req.user.email);
+    res.status(200).json({
+      status: "Success",
+      data: response
     });
+  });
 
-    authorize: RequestHandler = AsyncHandler(async (req, res, next) => {
-        const sessionData = req.session as SessionData || {};
-        const response = await this.walletService.authorize(req.body, sessionData);
-        res.status(200).json({
-            status: "Success",
-            message: "Charge on card initiated",
-            data: response
-        });
+  authorize: RequestHandler = AsyncHandler(async (req, res) => {
+    const sessionData = (req.session as SessionData) || {};
+    const response = await this.walletService.authorize(req.body, sessionData);
+    res.status(200).json({
+      status: "Success",
+      message: "Charge on card initiated",
+      data: response
     });
+  });
 
-    transfer: RequestHandler = AsyncHandler(async (req: ExtendRequest, res, next) => {
-        if (!req.user?.id)
-            throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
-        const response = await this.walletService.transfer(req.body, req.user.id);
-        res.status(200).json({
-            status: "Success",
-            message: "Transfer initiated",
-            data: response
-        });
+  transfer: RequestHandler = AsyncHandler(async (req: ExtendRequest, res) => {
+    if (!req.user?.id) {
+      throw new AppError("User is not authenticated.", "failed", false, StatusCodes.UNAUTHORIZED);
+    }
+    const response = await this.walletService.transfer(req.body, req.user.id);
+    res.status(200).json({
+      status: "Success",
+      message: "Transfer initiated",
+      data: response
     });
-
+  });
 }
