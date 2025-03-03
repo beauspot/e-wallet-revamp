fb48a327e1a40851a76d9ed9e3d01cb6579f57d9
beauspot <42305168+beauspot@users.noreply.github.com>
2025-03-03 13:56:25 +0100

    integrating eslint & prettier for code linting & formatting.

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,12 +1,11 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
-import { User } from "@/db/user.entity";
-import { protect } from "@/middlewares/authMiddleware";
-import { UserService } from "@/services/users.service";
-import { validate } from "@/helpers/middlewares/validate";
 import { UserController } from "@/controllers/users.controllers";
-
-import { createUserSchema, loginUserSchema } from "@/schemas/user.schema";
+import { User } from "@/db/user.entity";
+// import { validate } from "@/helpers/middlewares/validate";
+import { protect } from "@/middlewares/authMiddleware";
+// import { createUserSchema, loginUserSchema } from "@/schemas/user.schema";
+import { UserService } from "@/services/users.service";
 
 // Dependency injection
 const user_service = new UserService(User);
@@ -14,14 +13,30 @@ const user_controller = new UserController(user_service);
 
 const router = Router();
 
-router.route("/register").post((req: Request, res: Response, next: NextFunction) => user_controller.registerUser(req, res, next));
+router
+  .route("/register")
+  .post((req: Request, res: Response, next: NextFunction) =>
+    user_controller.registerUser(req, res, next)
+  );
 
-router.route("/verify-otp").post((req: Request, res: Response, next: NextFunction) => user_controller.verifyOTP(req, res, next));
+router
+  .route("/verify-otp")
+  .post((req: Request, res: Response, next: NextFunction) =>
+    user_controller.verifyOTP(req, res, next)
+  );
 
-router.route("/login").post((req: Request, res: Response, next: NextFunction) => user_controller.LoginUser(req, res, next));
+router
+  .route("/login")
+  .post((req: Request, res: Response, next: NextFunction) =>
+    user_controller.LoginUser(req, res, next)
+  );
 
 router.use(protect);
 
-router.route("/logout").post((req:Request, res: Response, next: NextFunction) => user_controller.LogoutUser(req, res, next));
+router
+  .route("/logout")
+  .post((req: Request, res: Response, next: NextFunction) =>
+    user_controller.LogoutUser(req, res, next)
+  );
 
-export default router
+export default router;
